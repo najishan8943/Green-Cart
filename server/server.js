514df85fcb0c05aa17d26ade2e0da1a5e1d9c2cc
Najisha <najisha200116@gmail.com>
2026-03-11 13:59:53 +0530

    Backend fixing

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const allowedOrigins = ['http://localhost:5173']
 //middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials:true}));
+app.use(cors({origin: true, credentials:true}));
 
 app.get('/',(req,res) => res.send("API is Working"));
 app.use('/api/user',userRouter)
